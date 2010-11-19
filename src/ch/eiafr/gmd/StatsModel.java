6ca280fada98ffd63b31873da3327c0b8d7e513c
wichtounet <baptistewicht@gmail.com>
2010-11-19 10:30:05 +0100

    Add virtual values to the model

--- src/ch/eiafr/gmd/StatsModel.java
+++ src/ch/eiafr/gmd/StatsModel.java
@@ -1,18 +1,23 @@
 package ch.eiafr.gmd;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-/**
- * Created by IntelliJ IDEA. User: wichtounet Date: Nov 5, 2010 Time: 11:40:58 AM To change this template use File |
- * Settings | File Templates.
- */
 public final class StatsModel implements Stats {
     private static final int INITIAL_CAPACITY = 25;
 
     private final Collection<StatsListener> listeners = new ArrayList<StatsListener>(2);
     private final List<Result> results = new ArrayList<Result>(INITIAL_CAPACITY);
+
+    public StatsModel() {
+        super();
+
+        results.add(new SimpleResult(2, 5));
+        results.add(new SimpleResult(10, 15));
+        results.add(new SimpleResult(22, 25));
+    }
 
     @Override
     public List<Result> getResults() {
@@ -32,6 +37,22 @@ public final class StatsModel implements Stats {
     private void fireStatsModified() {
         for (StatsListener listener : listeners) {
             listener.fireStatsModified();
+        }
+    }
+
+
+    private class SimpleResult implements Result {
+        private final List<Integer> integers;
+
+        private SimpleResult(Integer... integers) {
+            super();
+
+            this.integers = Arrays.asList(integers);
+        }
+
+        @Override
+        public List<Integer> getValues() {
+            return integers;
         }
     }
 }
