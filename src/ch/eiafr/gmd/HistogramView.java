95acc1a8dda247492585122423da27aaf2909058
jeremysingy <jeremy.singy@gmail.com>
2010-11-19 12:38:12 +0100

    Added a first implementation to drawing some histogram bars

--- src/ch/eiafr/gmd/HistogramView.java
+++ src/ch/eiafr/gmd/HistogramView.java
@@ -1,48 +1,77 @@
 package ch.eiafr.gmd;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.BorderFactory;
 
+/**
+ * View to draw an histogram graph
+ */
 public class HistogramView extends GraphView {
-    
+
     Stats stats;
-    
+    List<Drawable> drawableObjs;
+
     protected HistogramView(Stats stats) {
         super(stats);
 
         this.stats = stats;
+        drawableObjs = new ArrayList<Drawable>();
+        
         setBorder(BorderFactory.createBevelBorder(1));
+        
+        update();
     }
 
     @Override
     public void fireStatsModified() {
-        //if(stats.getResults().size() < 2)
-            //throw new IllegalStateException("There must be min 2 results in ");
+
+        // Update the objects and repaint the chart with new values
+        update();
+    }
+
+    public void update() {
+        drawableObjs = new ArrayList<Drawable>();
         
-        // Repaint the chart with new values
+        // TODO: handle this in a better way...
+        int posX = 2;
+
+        for(Result result : stats.getResults()) {
+            for(int value : result.getValues()) {
+                drawableObjs.add(new HistogramBar(Color.BLUE, posX, value));
+                
+                posX += 22;
+            }
+        }
+        
+        // TODO: adapt size
+        //setPreferredSize(new Dimension(500, getHeight()));
+        
         repaint();
     }
 
     @Override
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D)g;
-        List<Result> results = stats.getResults();
+
+        // Draw all objects
+        for(Drawable object : drawableObjs)
+            object.draw(g2d);
+    }
+    
+    private AffineTransform getCartesianCoordinateTransform(int h, Insets insets) {
+        int insetsTop = insets.top;
+
+        AffineTransform reposition = new AffineTransform();
+        reposition.translate(0, h + insetsTop);
+        reposition.scale(1, -1);
         
-        int i = 1;
-        
-        for(Result result : results) {
-            
-            List<Integer> values = result.getValues();
-            
-            System.out.println("n: " + results.size() + " i: " + i);
-            
-            g2d.drawString(values.get(0).toString(), 10.f, 10.f * i);
-            g2d.drawString(values.get(1).toString(), 60.f, 10.f * i);
-            
-            i++;
-        }
+        return reposition;
     }
 }
