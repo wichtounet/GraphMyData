5c4add1e68828ed609f37a07df14e8b087b4e8c3
Buttyx <buttyxavier@gmail.com>
2010-12-10 11:02:42 +0100

    Simplify the constructor and delete useless stuffs

--- src/ch/eiafr/gmd/PieChartView.java
+++ src/ch/eiafr/gmd/PieChartView.java
@@ -10,12 +10,10 @@ import java.awt.Graphics2D;
  *
  */
 public class PieChartView extends GraphView {
-    private final Stats stats;
+    
     
     protected PieChartView(Stats stats) {
         super(stats);
-
-        this.stats = stats;
     }
 
     @Override
@@ -30,9 +28,9 @@ public class PieChartView extends GraphView {
         super.paint(g);
         Graphics2D g2d = (Graphics2D)g;
         
-        int[][] values = new int[stats.getResults().size()][3];
+        int[][] values = new int[getStats().getResults().size()][3];
         int i = 0;
-        for(Result r:stats.getResults())
+        for(Result r:getStats().getResults())
         {
             for(int value:r.getValues())
             {
