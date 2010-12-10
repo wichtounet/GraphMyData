1583bcd3cd55d3e008433cc7fea5249211df5d09
Buttyx <buttyxavier@gmail.com>
2010-12-10 11:02:20 +0100

    Adding rectangle instead of colored text for the title and change method to round the end angle

--- src/ch/eiafr/gmd/PieFullCircle.java
+++ src/ch/eiafr/gmd/PieFullCircle.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Arc2D;
+import java.awt.geom.Rectangle2D;
 
 /**
  * Represent a full circle with different parts in pie chart
@@ -13,6 +14,13 @@ import java.awt.geom.Arc2D;
  */
 public class PieFullCircle implements Drawable{
     
+    private static int SIZE_REC = 8;
+    private static int MARGIN  = 12;
+    private static final Color[] COLORS = new Color[]{Color.YELLOW,Color.BLUE,Color.GREEN,
+                                                      Color.CYAN,Color.ORANGE,Color.RED,
+                                                      Color.WHITE,Color.GRAY,Color.DARK_GRAY,
+                                                      Color.LIGHT_GRAY, Color.PINK};
+      
     private int[]    parts;
     private String[] titles;
     private int   sum;
@@ -20,6 +28,7 @@ public class PieFullCircle implements Drawable{
     private int   height;
     private int   x;
     private int   y;
+    
     
     public PieFullCircle(String[] titles, int[] parts,  int x, int y, int w, int h){
         this.parts  = parts;
@@ -35,35 +44,50 @@ public class PieFullCircle implements Drawable{
     
     @Override
     public void draw(Graphics2D g2d) {
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         
+        // Antialiasing
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);     
         
-        Arc2D.Double tmpArc;
+        Arc2D.Double            tmpArc;
+        Rectangle2D.Double      tmpRec;
+        
         int startAngle = 0;
         int endAngle   = 0;
-        Color[] colors = new Color[]{Color.YELLOW,Color.BLUE,Color.GREEN,
-                                     Color.CYAN,Color.ORANGE,Color.RED,
-                                     Color.WHITE,Color.GRAY,Color.DARK_GRAY,
-                                     Color.LIGHT_GRAY, Color.PINK};
         
-        int pos=10;
+        int pos= MARGIN;
+        
+        // If there is something to draw
         if(sum!=0){
             int i=0;
+            
             // Create & draw each part
             for(int value:parts)
             {
+                // If no results for this case, we continue with the next one
                 if(value==0)continue;
-                endAngle = (int) (((double)360*((double)value/(double)sum))); 
+                
+                // Computing the angle
+                endAngle = (int) Math.round((((double)360*((double)value/(double)sum)))); 
+                
+                // Creation of the shapes
                 tmpArc   = new Arc2D.Double(x, y, width, height, startAngle, endAngle, Arc2D.PIE);
-                g2d.setPaint(colors[i%colors.length]);
+                tmpRec   = new Rectangle2D.Double(width+MARGIN,pos-MARGIN,SIZE_REC,SIZE_REC);
+                
+                // Fill the shapes with the good color
+                g2d.setPaint(COLORS[i%COLORS.length]);
                 g2d.fill(tmpArc);
-                g2d.drawString(titles[i], width+6, pos);
-                pos+=20;
+                g2d.fill(tmpRec);
+                
+                // Drawing
                 g2d.setPaint(Color.BLACK);
                 g2d.setStroke(new BasicStroke(1));
-                startAngle = endAngle+startAngle;
-                i++;
                 g2d.draw(tmpArc);
+                g2d.draw(tmpRec);
+                g2d.drawString(titles[i], width+2*MARGIN, pos);
+                
+                startAngle = endAngle+startAngle;
+                pos       += 20;
+                i++;
             }
         }
         
