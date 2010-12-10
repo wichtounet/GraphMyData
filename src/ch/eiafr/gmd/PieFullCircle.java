e5313f1f8b1ed85be5038675c06cac49a1c9a14e
Buttyx <buttyxavier@gmail.com>
2010-12-10 11:57:38 +0100

    Simplification of this classes by removing the part concerning the legend. Bug who used to make "PAC-MAN-like" chart resolved by using MATH.round.

--- src/ch/eiafr/gmd/PieFullCircle.java
+++ src/ch/eiafr/gmd/PieFullCircle.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Arc2D;
-import java.awt.geom.Rectangle2D;
 
 /**
  * Represent a full circle with different parts in pie chart
@@ -14,15 +13,13 @@ import java.awt.geom.Rectangle2D;
  */
 public class PieFullCircle implements Drawable{
     
-    private static int SIZE_REC = 8;
-    private static int MARGIN  = 12;
-    private static final Color[] COLORS = new Color[]{Color.YELLOW,Color.BLUE,Color.GREEN,
+
+    public  static final Color[] COLORS = new Color[]{Color.YELLOW,Color.BLUE,Color.GREEN,
                                                       Color.CYAN,Color.ORANGE,Color.RED,
                                                       Color.WHITE,Color.GRAY,Color.DARK_GRAY,
                                                       Color.LIGHT_GRAY, Color.PINK};
       
     private int[]    parts;
-    private String[] titles;
     private int   sum;
     private int   width;
     private int   height;
@@ -30,9 +27,8 @@ public class PieFullCircle implements Drawable{
     private int   y;
     
     
-    public PieFullCircle(String[] titles, int[] parts,  int x, int y, int w, int h){
+    public PieFullCircle(int[] parts,  int x, int y, int w, int h){
         this.parts  = parts;
-        this.titles = titles;
         this.width  = w;
         this.height = h;
         this.x      = x;
@@ -49,12 +45,10 @@ public class PieFullCircle implements Drawable{
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);     
         
         Arc2D.Double            tmpArc;
-        Rectangle2D.Double      tmpRec;
         
         int startAngle = 0;
         int endAngle   = 0;
         
-        int pos= MARGIN;
         
         // If there is something to draw
         if(sum!=0){
@@ -67,26 +61,21 @@ public class PieFullCircle implements Drawable{
                 if(value==0)continue;
                 
                 // Computing the angle
-                endAngle = (int) (((double)360*((double)value/(double)sum))); 
+                endAngle = (int) Math.round(((double)360*((double)value/(double)sum))); 
                 
-                // Creation of the shapes
+                // Creation of the shape
                 tmpArc   = new Arc2D.Double(x, y, width, height, startAngle, endAngle, Arc2D.PIE);
-                tmpRec   = new Rectangle2D.Double(width+MARGIN,pos-MARGIN,SIZE_REC,SIZE_REC);
                 
-                // Fill the shapes with the good color
+                // Fill the shape 
                 g2d.setPaint(COLORS[i%COLORS.length]);
                 g2d.fill(tmpArc);
-                g2d.fill(tmpRec);
-                
+
                 // Drawing
                 g2d.setPaint(Color.BLACK);
                 g2d.setStroke(new BasicStroke(1));
                 g2d.draw(tmpArc);
-                g2d.draw(tmpRec);
-                g2d.drawString(titles[i], width+2*MARGIN, pos);
-                
+
                 startAngle = endAngle+startAngle;
-                pos       += 20;
                 i++;
             }
         }
