85bfe0799f1bc8f251da3a0ef81aea5372c26d40
jeremysingy <jeremy.singy@gmail.com>
2010-12-19 16:55:51 +0100

    Added antialiasing for everything that is drawn on the pie chart (now legends render better)

--- src/ch/eiafr/gmd/PieChartView.java
+++ src/ch/eiafr/gmd/PieChartView.java
@@ -1,40 +1,38 @@
 package ch.eiafr.gmd;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
 /**
  * Class to display a pie chart 
  * 
  * @author ButtyX
- *
  */
 public class PieChartView extends GraphView {
     private static final int MARGIN   = 10;
     private static final int SIZE_REC = 8;
-    
+
     protected PieChartView(Stats stats) {
         super(stats);
     }
 
     @Override
     public void fireStatsModified() {        
-        
         // Repaint the pie chart with the new value
         repaint();
     }
-    
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
 
         int[][] results = getValues();
 
-        if(results.length > 0){
+        if(results.length > 0) {
             Graphics2D g2d = (Graphics2D) g;
+
+            // Antialiasing
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
             // Draw the legend of the pie charts
             String[] titles = {"< 10", "< 25", "> 25"};
@@ -58,60 +56,57 @@ public class PieChartView extends GraphView {
             }
         }
     }
-    
+
     /**
      * Get the values for the pie chart from the results
      * @return the values for each application
      */
-    private int[][] getValues()
-    {
-        if(getStats().getResults().isEmpty()){
+    private int[][] getValues() {
+        if(getStats().getResults().isEmpty()) {
             return new int[0][0];
         }
 
         int[][] values = new int[getStats().getResults().get(0).getValues().size()][3];
         int i = 0;
-        for(Result r:getStats().getResults())
-        {
+        for(Result r:getStats().getResults()) {
             i = 0;
-            for(int value:r.getValues())
-            {
+            for(int value:r.getValues()) {
                 if(value<10) {
                     values[i][0]++;
-                } else if(value<25) {
+                }
+                else if(value<25) {
                     values[i][1]++;
-                } else {
+                }
+                else {
                     values[i][2]++;
                 }
 
                 i++;
             }
         }
-        
+
         return values;
     }
-    
+
     /**
      * Draw the legend with the given titles
      * @param titles
      * @param g2d
      */
-    private void drawLegend(String[] titles, Graphics2D g2d)
-    {
-            Rectangle2D.Double      tmpRec;
-            int pos   = MARGIN;
-            for(int i=0;i<titles.length;i++){
-                tmpRec   = new Rectangle2D.Double(MARGIN,pos-MARGIN,SIZE_REC,SIZE_REC);
-                g2d.setPaint(PieFullCircle.COLORS[i%PieFullCircle.COLORS.length]);
-                g2d.fill(tmpRec);
-                
-                // Drawing
-                g2d.setPaint(Color.BLACK);
-                g2d.setStroke(new BasicStroke(1));
-                g2d.draw(tmpRec);
-                g2d.drawString(titles[i], 2*MARGIN, pos);
-                pos       += 20;
+    private void drawLegend(String[] titles, Graphics2D g2d) {
+        Rectangle2D.Double      tmpRec;
+        int pos   = MARGIN;
+        for(int i=0;i<titles.length;i++) {
+            tmpRec   = new Rectangle2D.Double(MARGIN,pos-MARGIN,SIZE_REC,SIZE_REC);
+            g2d.setPaint(PieFullCircle.COLORS[i%PieFullCircle.COLORS.length]);
+            g2d.fill(tmpRec);
+
+            // Drawing
+            g2d.setPaint(Color.BLACK);
+            g2d.setStroke(new BasicStroke(1));
+            g2d.draw(tmpRec);
+            g2d.drawString(titles[i], 2*MARGIN, pos);
+            pos       += 20;
         }
     }
-    
 }
--- src/ch/eiafr/gmd/PieFullCircle.java
+++ src/ch/eiafr/gmd/PieFullCircle.java
@@ -3,7 +3,6 @@ package ch.eiafr.gmd;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.geom.Arc2D;
 
 /**
@@ -11,61 +10,57 @@ import java.awt.geom.Arc2D;
  * 
  * @author ButtyX
  */
-public class PieFullCircle implements Drawable{
-    
+public class PieFullCircle implements Drawable {
 
-    public  static final Color[] COLORS = new Color[]{Color.YELLOW,Color.BLUE,Color.GREEN,
-                                                      Color.CYAN,Color.ORANGE,Color.RED,
-                                                      Color.WHITE,Color.GRAY,Color.DARK_GRAY,
-                                                      Color.LIGHT_GRAY, Color.PINK};
-      
-    private int[]    parts;
+
+    public static final Color[] COLORS = new Color[]{Color.YELLOW,Color.BLUE,Color.GREEN,
+        Color.CYAN,Color.ORANGE,Color.RED,
+        Color.WHITE,Color.GRAY,Color.DARK_GRAY,
+        Color.LIGHT_GRAY, Color.PINK};
+
+    private int[] parts;
     private int   sum;
     private int   width;
     private int   height;
     private int   x;
     private int   y;
-    
-    
-    public PieFullCircle(int[] parts,  int x, int y, int w, int h){
+
+
+    public PieFullCircle(int[] parts,  int x, int y, int w, int h) {
         this.parts  = parts;
         this.width  = w;
         this.height = h;
         this.x      = x;
         this.y      = y;
-        
+
         // Compute the sum of all the parts
-        for(int i:parts)sum+=i;
+        for(int i : parts)
+            sum+=i;
     }
-    
+
     @Override
     public void draw(Graphics2D g2d) {
-        
-        // Antialiasing
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);     
-        
-        Arc2D.Double            tmpArc;
-        
+        Arc2D.Double tmpArc;
+
         int startAngle = 0;
         int endAngle   = 0;
-        
-        
+
         // If there is something to draw
-        if(sum!=0){
+        if(sum != 0) {
             int i=0;
-            
+
             // Create & draw each part
-            for(int value:parts)
-            {
+            for(int value:parts) {
                 // If no results for this case, we continue with the next one
-                if(value==0)continue;
-                
+                if(value == 0)
+                    continue;
+
                 // Computing the angle
-                endAngle = (int) Math.round(((double)360*((double)value/(double)sum))); 
-                
+                endAngle = (int)Math.round(((double)360*((double)value/(double)sum))); 
+
                 // Creation of the shape
                 tmpArc   = new Arc2D.Double(x, y, width, height, startAngle, endAngle, Arc2D.PIE);
-                
+
                 // Fill the shape 
                 g2d.setPaint(COLORS[i%COLORS.length]);
                 g2d.fill(tmpArc);
@@ -79,7 +74,7 @@ public class PieFullCircle implements Drawable{
                 i++;
             }
         }
-        
+
     }
 
 }
